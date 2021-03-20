c882b83cdde08eaea561392a06d8d55914d7ebe9
agimaulana <agimaulana.dex@gmail.com>
2021-03-21 00:23:43 +0700

    reactive to get total expenses and total remaining

--- app/src/main/java/io/github/agimaulana/monity/viewmodel/DailyExpenseViewModel.kt
+++ app/src/main/java/io/github/agimaulana/monity/viewmodel/DailyExpenseViewModel.kt
@@ -1,18 +1,16 @@
 package io.github.agimaulana.monity.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.github.agimaulana.monity.extensions.mutableLiveData
 import io.github.agimaulana.monity.model.DailyExpense
 import io.github.agimaulana.monity.model.TotalDailyExpense
 import io.github.agimaulana.monity.repository.DailyExpenseRepository
 import io.github.agimaulana.monity.repository.DailyRationRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
@@ -25,14 +23,26 @@ class DailyExpenseViewModel @Inject constructor(
                 .asLiveData(Dispatchers.IO)
     }
 
+    @FlowPreview
     val totalRemaining: LiveData<TotalDailyExpense> by lazy {
         collectTotalDailyExpense().asLiveData(Dispatchers.IO)
     }
 
-    private fun collectTotalDailyExpense() = dailyRationRepository.get()
-            .zip(dailyExpenseRepository.getAll()) { dailyRation, expenses ->
-                val totalExpenses = expenses.sumOf { it.amount }
-                val remaining = (dailyRation?.amount ?: 0) - expenses.sumOf { it.amount }
-                TotalDailyExpense(totalExpenses, remaining)
+//    private fun collectTotalDailyExpense() = dailyRationRepository.get()
+//            .distinctUntilChanged()
+//            .zip(dailyExpenseRepository.getAll().distinctUntilChanged()) { dailyRation, expenses ->
+//                val totalExpenses = expenses.sumOf { it.amount }
+//                val remaining = (dailyRation?.amount ?: 0) - expenses.sumOf { it.amount }
+//                TotalDailyExpense(totalExpenses, remaining)
+//            }
+
+    @FlowPreview
+    private fun collectTotalDailyExpense() = dailyExpenseRepository.getAll()
+            .flatMapMerge { expenses ->
+                dailyRationRepository.get().map { dailyRation ->
+                    val totalExpenses = expenses.sumOf { it.amount }
+                    val remaining = (dailyRation?.amount ?: 0) - expenses.sumOf { it.amount }
+                    TotalDailyExpense(totalExpenses, remaining)
+                }
             }
 }
